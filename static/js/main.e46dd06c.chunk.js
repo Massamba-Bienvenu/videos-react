4983f6752e5e238b761cf74a7efd7742dab8394d
Bienvenu Massamba <boukamass@gmail.com>
2021-01-28 12:11:16 -0600

    Updates

---
+++ static/js/main.e46dd06c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpvids=this.webpackJsonpvids||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c(1),s=c.n(r),i=c(3);var d=function(){return Object(n.jsx)("div",{children:Object(n.jsx)("h2",{children:"Hello from React"})})};c.n(i).a.render(Object(n.jsx)(s.a.StrictMode,{children:Object(n.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.e46dd06c.chunk.js.map

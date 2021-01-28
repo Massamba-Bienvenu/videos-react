ae4e7faa9947227e2ed98de85eb3959aba754bca
Bienvenu Massamba <boukamass@gmail.com>
2021-01-28 14:16:54 -0600

    Finished axios connection to api and tested

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ In this project the semantic UI library for used.
 ## VIDEOS API
 
 To get the videos we used the youtube api (console.developers.google.com)
-key: AIzaSyClqhBbsPugwao8Q211BfCs4evjsspUX7w
 The axios library was used to send and receive data from youtube.
-
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube.js";
 
 export class App extends Component {
   state = {
+    videos: [],
     term: "",
   };
 
@@ -10,10 +12,18 @@ export class App extends Component {
     this.setState({ term: event.target.value });
   };
 
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
+
   handleOnFormSubmit = (event) => {
     event.preventDefault();
-    // TO DO MAKE SURE WE CALL CALL BACK FROM PRESENT COMP
-    
+    this.onTermSubmit(this.state.term);
   };
 
   render() {
@@ -24,6 +34,7 @@ export class App extends Component {
           OnInputChange={this.handleOnInputChange}
           onFormSubmit={this.handleOnFormSubmit}
         />
+        I have: {this.state.videos.length}
       </div>
     );
   }
